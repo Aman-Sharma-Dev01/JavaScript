50e3db29af0853d84ccba8d5b80204f0a8b0ed49
Aman Sharma <asharma8464@gmail.com>
2024-12-21 10:50:00 +0000

    datatypes

---
+++ JavaScript/03_datatypes.js
@@ -0,0 +1,18 @@
+let name = "Aman"
+let age = 20
+let isRegister = true
+let address = null
+let coure = undefined
+
+// there are some imp datatypes in javascript
+
+// string = "aman"
+// number = 234
+// boolean = true/false
+// null = empty /( (the type of null is object.) imp...!
+// undefine = not defined
+
+console.table([typeof name, typeof age, typeof isRegister, typeof address , typeof coure]);
+
+//type of is used to print the type of any variable.
+
