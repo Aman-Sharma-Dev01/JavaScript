753eb5496251a99d9c61818e24b43f874bbab8c2
Aman Sharma <asharma8464@gmail.com>
2024-12-21 11:13:25 +0000

    datatypes conversion

---
+++ JavaScript/04_datatypesConversion.js
@@ -0,0 +1,33 @@
+let score = "45abc"
+
+let numString = Number(score)
+console.log(numString);
+
+/*
+ 45 => "45"
+ "45" => 45
+ "45abc" => NaN (Not a number)
+ */
+
+ let bool = true
+
+ let boolnum = Number(bool)
+ console.log(boolnum);
+
+ /*
+  ture => 1
+  false => 0
+  */
+
+  let str = ""
+
+ let boolstr = Boolean(str)
+ console.log(boolstr);
+
+ /*
+   "" => false
+  "aman" => true
+  */
+
+ 
+ 
