8f27fc3e49c73b22bc351f5be0724bc02ea1108f
Aman Sharma <asharma8464@gmail.com>
2024-12-23 15:18:12 +0000

    num and maths

---
+++ JavaScript/06_Num_and_Maths.js
@@ -0,0 +1,27 @@
+let balance = 100
+console.log(balance);
+
+console.log(balance.toString()); //used to convert number into String.
+console.log(balance.toFixed(2)) // used to add point value like 100.00.
+
+
+/*********************************** Maths ***********************************/
+
+// console.log(Math.round(99.9)); //used to roundoff the give floating point number. 
+// console.log(Math.ceil(99.9)); 
+// console.log(Math.floor(99.9));
+console.log(Math.min(3,4,5,6,7)); // used to find the smallest number.
+console.log(Math.max(3,4,5,6,7)); // used to find the biggest number.
+
+
+// console.log(Math.random()) // used to take a number between 0 to 1.
+
+let min = 20
+let max = 40
+
+let final_num = (Math.random() * (max - min + 1)+20); // it will give a number between 20 t0 40.
+
+console.log(final_num)
+
+
+
