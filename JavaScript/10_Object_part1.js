9b056d8191aa22c686ccc9b790fa53b5287e779b
Aman Sharma <asharma8464@gmail.com>
2024-12-27 09:48:58 +0000

    objects

---
+++ JavaScript/10_Object_part1.js
@@ -0,0 +1,33 @@
+/*********************** Objects *************************/
+let sym = Symbol("Key")
+const user1 = {
+    name : "Aman" ,
+    "full name" : "Aman Sharma" ,
+    age : 20 ,
+    location : "Faridabad" ,
+    "email" : "aman@gmail.com" ,
+    isLoggedin : true ,
+    lastLoginDays : ["Mon" , "Wed" , "Fri" , "Sat"] ,
+    [sym] : "key1"
+
+}
+
+// console.log(user1);
+console.log(user1.name); // not a good practice.
+console.log(user1["full name"]); // Good practice.
+console.log(user1[sym])
+
+user1.email = "aman@new.com"
+// Object.freeze(user1) // used to freeze the object and we cannot change any value of the object.
+user1["full name"] = "king"
+console.log(user1["full name"]);
+
+user1.greeting = function(){
+    console.log(`Hello world my name is ${this.name} and i am ${this.age} years old.`);
+    
+}
+
+console.log(user1.greeting());
+
+
+
---
+++ JavaScript/11_Object_part2.js
@@ -0,0 +1,70 @@
+// let anObj = new Object() // it is a singleton object
+// anObj.name = "aman"
+// console.log(anObj.name);
+
+let instagramUser = {} // non singleton object.
+instagramUser.Id = "123abc"
+instagramUser.name = "Aman"
+instagramUser.isLoggedIn = true
+
+console.log(instagramUser);
+
+
+//nested object is possible 
+
+const anotherapp = {
+    Id : "456def" ,
+    name : {
+        username : {
+            fullname : {
+                firstname : "Aman" ,
+                lastname : "Sharma"
+            }
+        } 
+    }
+}
+
+console.log(anotherapp.name.username.fullname);
+
+let obj1 = { 1 : "a" , 2 : "b"}
+let obj2 = { 3 : "c" , 4 : "d"}
+
+let obj3 = Object.assign(obj1 , obj2)
+console.log(obj3);
+
+// We can also use spread method to cobime 2 or more objects.
+
+let obj4 = {...obj1 , ...obj2}
+console.log(obj4);
+
+let userdata = [
+    {
+        userid : 1 ,
+        username : "aman"
+    },
+    {
+        userid : 1 ,
+        username : "aman"
+    },
+    {
+        userid : 1 ,
+        username : "aman"
+    },
+    {
+        userid : 1 ,
+        username : "aman"
+    }
+]
+
+console.log(userdata[1].userid)
+
+/************** Imp Methods **************/
+
+console.log(Object.keys(instagramUser)); // it will print all the keys of the object.
+console.log(Object.values(instagramUser)); // it will print all the values of the object.
+console.log(Object.entries(instagramUser)); // it will give all the keys and value saperated in the array.
+
+console.log(Object.hasOwnProperty('name'));
+
+
+
