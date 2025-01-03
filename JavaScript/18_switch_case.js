607166fa580869ea6a5ef2e71398efe665fd3554
Aman Sharma <asharma8464@gmail.com>
2025-01-03 12:29:00 +0000

    switch case

---
+++ JavaScript/18_switch_case.js
@@ -0,0 +1,55 @@
+//switch case is used to check the multiple conditions at a time.
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 9
+
+switch (month){
+    case 1:
+        console.log("jan");
+    break
+
+    case 2:
+        console.log("feb");
+    break
+    case 3:
+        console.log("march");
+    break
+    case 4:
+        console.log("april");
+    break
+    case 5:
+        console.log("may");
+    break
+    case 6:
+        console.log("june");
+    break
+    case 7:
+        console.log("july");
+    break
+    case 8:
+        console.log("august");
+    break
+    case 9:
+        console.log("sep");
+    break
+    case 10:
+        console.log("oct");
+    break
+    case 11:
+        console.log("nov");
+    break
+    case 12:
+        console.log("dec");
+    break
+    default:
+        console.log("invalid input");
+    break
+}
